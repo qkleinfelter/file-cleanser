e1c695cf2a0d8a2cf5ba0a5abc5213302536af04
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-06-19 11:12:32 -0400

    Add winston logging

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 let fs = require('fs');
 const config = require('config');
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'app.log' })
+    ]
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+}
+
 let numFiles = 0;
 let fileSize = 0;
 
@@ -22,25 +38,25 @@ function cleanFiles(dir, timepassed) {
                 numFiles++;
                 fileSize += fs.statSync(filePath).size;
                 if (config.get('mode') == 'log') {
-                    console.log(filePath + " is older than the configured timepassed")
+                    logger.info(filePath + " is older than the configured timepassed")
                 }
                 else if (config.get('mode') == 'deletion') {
                     fs.unlink(filePath);
-                    console.log(filePath +" was deleted because it was older than the configured timepassed")
+                    logger.info(filePath +" was deleted because it was older than the configured timepassed")
                 }
             }
             
         }
     })
-    printStats()
 }
 
 let printStats = () => {
-    console.log(`We encountered ${numFiles} files that were passed the specified time limit`);
-    console.log(`Those files used ${fileSize / 1024 / 1024 / 1024} Gigabytes of storage`);
+    logger.info(`We encountered ${numFiles} files that were passed the specified time limit`);
+    logger.info(`Those files used ${fileSize / 1024 / 1024 / 1024} Gigabytes of storage`);
 }
 
 let directory = config.get('directory');
 let timepassed = config.get('timepassed');
+logger.log('warn', `BEGIN RUN: MODE=${config.get('mode')}, DIRECTORY=${directory}, TIMEPASSED=${timepassed}`)
 cleanFiles(directory, timepassed);
 setTimeout(printStats, 1000);
