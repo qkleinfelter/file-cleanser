8f69de07d3fef7661bce505af1cc8b0de86f3309
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-06-19 10:59:56 -0400

    Updates to handle different modes and a bit of logging

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 let fs = require('fs');
 const config = require('config');
+let numFiles = 0;
+let fileSize = 0;
 
 let getFileModTime = (path) => {
     const stats = fs.statSync(path);
     return stats.mtime;
 };
 
-let cleanFiles = (dir, timepassed) => {
+function cleanFiles(dir, timepassed) {
     fs.readdir(dir, (err, files) => {
         if (err) {
             console.error("Could not list the directory.", err);
             process.exit(1);
         }
 
-        files.forEach((file, index) => {
+        for (const file of files) {
             const filePath = dir + "\\" + file;
             const time = new Date(getFileModTime(filePath)).getTime();
             if (Date.now() - time > timepassed) {
-                console.log(filePath + " is older than the configured timepassed, it would be deleted if running in deletion mode")
+                numFiles++;
+                fileSize += fs.statSync(filePath).size;
+                if (config.get('mode') == 'log') {
+                    console.log(filePath + " is older than the configured timepassed")
+                }
+                else if (config.get('mode') == 'deletion') {
+                    fs.unlink(filePath);
+                    console.log(filePath +" was deleted because it was older than the configured timepassed")
+                }
             }
             
-        })
+        }
     })
+    printStats()
+}
+
+let printStats = () => {
+    console.log(`We encountered ${numFiles} files that were passed the specified time limit`);
+    console.log(`Those files used ${fileSize / 1024 / 1024 / 1024} Gigabytes of storage`);
 }
 
 let directory = config.get('directory');
 let timepassed = config.get('timepassed');
 cleanFiles(directory, timepassed);
+setTimeout(printStats, 1000);
